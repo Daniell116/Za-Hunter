2b1111b4d15a2241a297ce6e9f3de62d0dc3a387
daniell escareno <daniellescareno7@gmail.com>
2026-02-10 08:37:06 -0800

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by daniell Escareno on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
